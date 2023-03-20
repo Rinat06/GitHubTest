6f62ceecdabe81839cd61528356ae54f5c1013db
Rinat06 <110852397+Rinat06@users.noreply.github.com>
2023-03-21 01:51:38 +0300

    sum

--- app/src/main/java/com/example/githubtest/MainActivity.kt
+++ app/src/main/java/com/example/githubtest/MainActivity.kt
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    var a = 2
+    var b = 4
+    var resultat = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        resultat = a + b
     }
 }
